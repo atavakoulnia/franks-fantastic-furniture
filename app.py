f90c948e979bc7bddabe47343d45f93a4b87f5e7
Armonymon <71794386+Armonymon@users.noreply.github.com>
2021-07-30 13:38:24 -0700

    Take 3

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, url_for
+import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
 
 # Routes 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/customers')
+def customers():
+    return render_template('customers.html')
 
 # Listener
 
