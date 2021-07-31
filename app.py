097980b84f22bb390743c380f2d764eadc891fb4
Armonymon <71794386+Armonymon@users.noreply.github.com>
2021-07-31 14:34:21 -0700

    Take 5

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for, request
 import database.db_connector as db
-
-# Configuration
-
+# ---------------------------------------------------------------------------------------
+#                                   Configuration
+# ---------------------------------------------------------------------------------------
 app = Flask(__name__)
 db_connection = db.connect_to_database()
-
-# Routes 
-
+# ---------------------------------------------------------------------------------------
+#                                       Routes 
+# ---------------------------------------------------------------------------------------
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,25 +17,42 @@ def customers():
     query = "SELECT * FROM customers;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
-    return render_template('customers.html')
+    return render_template('customers.html', customers=results)
 
-@app.route('/orders')
+@app.route('/orders', methods=['POST', 'GET'])
 def orders():
-    return render_template('orders.html')
+    query = "SELECT * FROM orders;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('orders.html', orders=results)
 
-@app.route('/transactions')
+@app.route('/transactions', methods=['POST', 'GET'])
 def transactions():
-    return render_template('transactions.html')
+    query = "SELECT * FROM transactions;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('transactions.html', transactions=results)
 
-@app.route('/products')
+@app.route('/products', methods=['POST', 'GET'])
 def products():
-    return render_template('products.html')
+    query = "SELECT * FROM products;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('products.html', products=results)
 
-@app.route('/employees')
+@app.route('/employees', methods=['POST', 'GET'])
 def employees():
-    return render_template('employees.html')
-
-# Listener
-
+    query = "SELECT * FROM employees;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template('employees.html', employees=results)
+# ---------------------------------------------------------------------------------------
+@app.route('/form', methods=['POST'])
+def form():
+    title = "Thank you, attributes have been successfully added! (not really though)"
+    return render_template('form.html', title=title)
+# ---------------------------------------------------------------------------------------
+#                                       Listener
+# ---------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
