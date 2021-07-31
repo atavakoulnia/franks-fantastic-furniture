9d5f99f4e8f8bf22e6c4ab436534b6e26d433b65
Armonymon <71794386+Armonymon@users.noreply.github.com>
2021-07-31 03:48:04 -0700

    Take 4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import database.db_connector as db
 
 # Configuration
@@ -12,9 +12,28 @@ db_connection = db.connect_to_database()
 def index():
     return render_template('index.html')
 
-@app.route('/customers')
+@app.route('/customers', methods=['POST', 'GET'])
 def customers():
+    query = "SELECT * FROM customers;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
     return render_template('customers.html')
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+@app.route('/transactions')
+def transactions():
+    return render_template('transactions.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/employees')
+def employees():
+    return render_template('employees.html')
 
 # Listener
 
